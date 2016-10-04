b4a5a6d7d9e2bfd46d7d70733e2834f6b84dba88
kotbegemot <k0tb9g9m0t@yandex.ru>
2016-10-04 13:12:35 +0300

    changed interface design

--- libactor_zeta_io/actor-zeta/network/multiplexer.hpp
+++ libactor_zeta_io/actor-zeta/network/multiplexer.hpp
@@ -11,7 +11,9 @@ namespace actor_zeta {
 
             virtual ~multiplexer() = default;
 
-            virtual void new_tcp_interfaces(const std::string &host, uint16_t port) = 0;
+            virtual void new_tcp_listener(const std::string &host, uint16_t port) = 0;
+
+            virtual void new_tcp_connection(const std::string &host, uint16_t port) = 0;
 
             virtual void close(const connection_identifying &) = 0;
 
