53bf8ca71752d54920b743bbaa6f74ed77c5bff8
Anastasiia Patenkova <a.patenkova@3opinion.ai>
2021-06-14 19:07:13 +0300

    communication_module fix

--- examples/add_method/main.cpp
+++ examples/add_method/main.cpp
@@ -3,67 +3,64 @@
 #include <set>
 #include <string>
 
-#include <actor-zeta/core.hpp>
+#include <actor-zeta.hpp>
 
 using actor_zeta::basic_async_actor;
-using actor_zeta::supervisor;
-using actor_zeta::message;
 using actor_zeta::execution_device;
+using actor_zeta::message_ptr;
+using actor_zeta::supervisor_t;
 
-class dummy_supervisor final : public supervisor {
+class dummy_supervisor final : public supervisor_t {
 public:
-    dummy_supervisor(): supervisor("dummy_supervisor"){}
+    dummy_supervisor()
+        : supervisor_t("dummy_supervisor") {}
 
-    auto executor() noexcept -> actor_zeta::abstract_executor& override  {
-        return *ptr_;
+    auto executor() noexcept -> actor_zeta::abstract_executor* override {
+        return ptr_;
     }
 
     auto join(actor_zeta::actor) -> actor_zeta::actor_address override {
         return actor_zeta::actor_address();
     }
 
-    void enqueue(message, execution_device *) override {}
+    void enqueue_base(message_ptr, actor_zeta::execution_device*) override {}
 
 private:
-    actor_zeta::abstract_executor*ptr_ = nullptr;
+    actor_zeta::abstract_executor* ptr_ = nullptr;
 };
 
 class storage_t final : public basic_async_actor {
 public:
-    storage_t(dummy_supervisor&ref) : basic_async_actor(ref, "storage") {
+    storage_t(dummy_supervisor& ref)
+        : basic_async_actor(&ref, "storage") {
         add_handler(
-                "update",
-                [](communication_module & /*ctx*/) -> void {}
-        );
+            "update",
+            []() -> void {});
 
         add_handler(
-                "find",
-                [](communication_module & /*ctx*/) -> void {}
-        );
+            "find",
+            []() -> void {});
 
         add_handler(
-                "remove",
-                [](communication_module & /*ctx*/ ) -> void {}
-        );
+            "remove",
+            []() -> void {});
     }
 
     ~storage_t() override = default;
 };
 
-
 int main() {
+    auto* supervisor = new dummy_supervisor;
 
-    auto * supervisor = new dummy_supervisor;
-
-    auto *storage_tmp = new storage_t(*supervisor);
+    auto* storage_tmp = new storage_t(*supervisor);
 
     actor_zeta::actor storage(storage_tmp);
 
-    assert(actor_zeta::detail::string_view("storage") == storage->name());
+    //assert(actor_zeta::detail::string_view("storage") == storage->name());
 
     auto tmp = storage->message_types();
 
-    std::set<std::string> control = {"add_link","remove_link", "update", "remove", "find"};
+    std::set<std::string> control = {"add_link", "remove_link", "update", "remove", "find"};
 
     std::set<std::string> diff;
 
