0d446bcbaf0fcb6d5fd936b361ce7f192b5f34b0
0xBYTESHIFT <0xbyteshift@gmail.com>
2021-06-18 17:27:07 +0300

    update for spawn

--- examples/spawn/main.cpp
+++ examples/spawn/main.cpp
@@ -1,4 +1,5 @@
 #include "link.hpp"
+#include "send.hpp"
 #include <cassert>
 
 #include <chrono>
@@ -12,17 +13,20 @@
 #include <actor-zeta.hpp>
 
 using actor_zeta::abstract_executor;
-using actor_zeta::context;
 
 using actor_zeta::abstract_executor;
 using actor_zeta::executor_t;
 using actor_zeta::work_sharing;
 
 using actor_zeta::make_message;
+using namespace std::chrono_literals;
+
+std::atomic_int count;
+const int max_queue = 100;
 
 template<typename Task, typename... Args>
 inline auto make_task(actor_zeta::supervisor& executor_, const std::string& command, Args... args) -> void {
-    executor_->enqueue(std::move(make_message(executor_->address(), command, std::move(Task(std::forward<Args>(args)...)))));
+    actor_zeta::send(executor_, executor_->address(), command, std::move(Task(std::forward<Args>(args)...)));
 }
 
 template<typename Task, typename... Args>
@@ -35,52 +39,50 @@ auto thread_pool_deleter = [](abstract_executor* ptr) {
     delete ptr;
 };
 
-const int max_queue = 100;
-struct spawn_task final {
-    spawn_task() = default;
-    ~spawn_task() = default;
-};
+class worker_t2;
+class worker_t3;
 
 class worker_t final : public actor_zeta::basic_async_actor {
 public:
     explicit worker_t(actor_zeta::supervisor_abstract* ptr)
-        : actor_zeta::basic_async_actor(ptr, "bot") {
-        add_handler("on_spawn", &worker_t::on_spawn);
+        : actor_zeta::basic_async_actor(ptr, "bot1") {
+        add_handler("spawn_worker", &worker_t::spawn_worker);
     }
 
-    ~worker_t() override = default;
-
-    /**
-     * this will be called by a supervisor when it spawns new actor and broadcasts it
-     * */
-    auto on_spawn(actor_zeta::basic_async_actor* ptr) -> void {
-        //worker should link only with worker, here we check what we got from a broadcast
-        auto wrkr = dynamic_cast<worker_t*>(ptr); //what to do with rtti?
-        if (wrkr) {
-            actor_zeta::link(*this, wrkr);
-        } else {
-            std::cout << "not my type, won't link\n";
-        }
+    auto spawn_worker() -> void {
+        actor_zeta::spawn_actor<worker_t2>(addresses("manager-2"));
+        actor_zeta::spawn_actor<worker_t3>(addresses("manager-2"));
+        count++;
     }
-
-private:
 };
 
-/// non thread safe
+class worker_t2 final : public actor_zeta::basic_async_actor {
+public:
+    explicit worker_t2(actor_zeta::supervisor_abstract* ptr)
+        : actor_zeta::basic_async_actor(ptr, "bot2") {
+        std::cout << "bot2 created\n";
+    }
+};
+
+class worker_t3 final : public actor_zeta::basic_async_actor {
+public:
+    explicit worker_t3(actor_zeta::supervisor_abstract* ptr)
+        : actor_zeta::basic_async_actor(ptr, "bot3") {
+        std::cout << "bot3 created\n";
+    }
+};
+
 class supervisor_lite final : public actor_zeta::supervisor_abstract {
 public:
-    explicit supervisor_lite()
-        : supervisor_abstract("network")
+    explicit supervisor_lite(std::string name)
+        : supervisor_abstract(std::move(name))
         , e_(new executor_t<work_sharing>(1, max_queue),
              thread_pool_deleter)
         , system_{"add_link",
                   "remove_link",
                   "spawn_actor"} {
-        add_handler("spawn_worker", &supervisor_lite::spawn_worker);
         e_->start();
     }
-
-    ~supervisor_lite() override = default;
 
     auto executor_impl() noexcept -> actor_zeta::abstract_executor* final { return e_.get(); }
 
@@ -92,12 +94,8 @@ public:
         supervisor_.emplace_back(std::move(t));
     }
 
-    auto spawn_worker(spawn_task& t_) -> void {
-        auto t = std::move(t_);
-        actor_zeta::spawn_actor<worker_t>(*this);
-    }
-
     auto enqueue_base(actor_zeta::message_ptr msg, actor_zeta::execution_device*) -> void final {
+        std::lock_guard<std::mutex> lock(mt);
         auto msg_ = std::move(msg);
         auto it = system_.find(msg_->command());
         if (it != system_.end()) {
@@ -111,27 +109,33 @@ private:
         execute(*this);
     }
 
+    std::mutex mt;
     std::unique_ptr<abstract_executor, decltype(thread_pool_deleter)> e_;
     std::vector<actor_zeta::actor> actors_;
     std::vector<actor_zeta::supervisor> supervisor_;
     std::unordered_set<actor_zeta::detail::string_view> system_;
 };
 
-using namespace std::chrono_literals;
-
 auto main() -> int {
-    actor_zeta::supervisor supervisor(new supervisor_lite());
+    //TODO: broadcast handling in actors when spawn happens in supervisor
+    //TODO: link existing actors with spawned ones when their type follows some predicate (based on name?)
+    //TODO: "manager-2" has to be known in "bot1" names scope
+    actor_zeta::supervisor supervisor1(new supervisor_lite("manager-1"));
+    actor_zeta::supervisor supervisor2(new supervisor_lite("manager-2"));
+    actor_zeta::link(supervisor1, supervisor2);
 
     int const actors = 5;
+    count = 0;
 
-    worker_t wrkr(supervisor.get()); //this actor will recieve broadcasts with actors from supervisor
+    actor_zeta::spawn_actor<worker_t>(supervisor1); //actor creator, "bot1"
 
     for (auto i = actors - 1; i > 0; --i) {
-        make_task<spawn_task>(supervisor, "spawn_worker"); //ask for spawns from supervisor
+        actor_zeta::send(supervisor1, "bot1", "spawn_worker");
     }
 
-    std::this_thread::sleep_for(180s);
-
+    std::this_thread::sleep_for(10s);
     std::cerr << " Finish " << std::endl;
+
+    assert(count == actors);
     return 0;
 }
