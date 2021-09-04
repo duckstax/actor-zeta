e9eec2345de0581441de151c37c2c0b5107d5a55
kotbegemot <k0tb9g9m0t@yandex.ru>
2021-09-05 02:34:45 +0300

    dynamic-spawn-supervisor

--- header/actor-zeta/spawn.hpp
+++ header/actor-zeta/spawn.hpp
@@ -22,7 +22,7 @@ namespace actor_zeta {
             auto allocate_byte = sizeof(Actor);
             auto allocate_byte_alignof = alignof(Actor);
             void* buffer = supervisor->resource()->allocate(allocate_byte, allocate_byte_alignof);
-            return new (buffer) Actor(supervisor,(std::get<I>(args))...);
+            return new (buffer) Actor(supervisor, (std::get<I>(args))...);
         }
 
         template<
@@ -36,22 +36,39 @@ namespace actor_zeta {
             return new (buffer) ChildrenSupervisor(supervisor, (std::get<I>(args))...);
         }
 
-    template<
-        class Actor,
-        class... Args>
-    auto spawn_actor_impl(base::address_t recipient, base::address_t sender, Args&&... args) -> void {
-        using args_types = type_traits::type_list<Args...>;
-        static constexpr size_t number_of_arguments = type_traits::type_list_size<args_types>::value;
-        send(
-            std::move(recipient),
-            std::move(sender),
-            "spawn_actor",
-            std::forward<base::default_spawn_actor>(
+        template<
+            class Actor,
+            class... Args>
+        auto spawn_actor_impl(base::address_t recipient, base::address_t sender, Args&&... args) -> void {
+            using args_types = type_traits::type_list<Args...>;
+            static constexpr size_t number_of_arguments = type_traits::type_list_size<args_types>::value;
+            send(
+                std::move(recipient),
+                std::move(sender),
+                "spawn_actor",
                 base::default_spawn_actor(
-                    [args_ = std::move(std::tuple<Args&&...>(std::forward<Args&&>(args)...))](actor_zeta::base::supervisor_abstract* ptr) {
+                    [args_ = std::make_tuple(std::forward<Args&&>(args)...)](actor_zeta::base::supervisor_abstract* ptr) {
                         return detail::created_actor<Actor>(ptr, args_, type_traits::make_index_sequence<number_of_arguments>{});
-                    })));
-    }
+                    }));
+        }
+
+        template<
+            class ChildrenSupervisor,
+            class... Args,
+            class = type_traits::enable_if_t<std::is_base_of<supervisor_abstract, ChildrenSupervisor>::value>>
+        auto spawn_supervisor_impl(base::address_t recipient, base::address_t sender, Args&&... args) -> void {
+            using args_types = type_traits::type_list<Args...>;
+            static constexpr size_t number_of_arguments = type_traits::type_list_size<args_types>::value;
+            send(
+                std::move(recipient),
+                std::move(sender),
+                "spawn_supervisor",
+                std::move(
+                    base::default_spawn_supervisor(
+                        [args_ = std::make_tuple(std::forward<Args&&>(args)...)](actor_zeta::supervisor_abstract* ptr) {
+                            return detail::created_supervisor<ChildrenSupervisor>(ptr, args_, type_traits::make_index_sequence<number_of_arguments>{});
+                        })));
+        }
 
     } // namespace detail
 
@@ -74,18 +91,8 @@ namespace actor_zeta {
         class ChildrenSupervisor,
         class... Args,
         class = type_traits::enable_if_t<std::is_base_of<supervisor_abstract, ChildrenSupervisor>::value>>
-    auto spawn_supervisor(base::supervisor& supervisor, Args&&... args) -> void {
-        using args_types = type_traits::type_list<Args...>;
-        static constexpr size_t number_of_arguments = type_traits::type_list_size<args_types>::value;
-        send(
-            supervisor->address(),
-            supervisor->address(),
-            "spawn_supervisor",
-            std::move(
-                base::default_spawn_supervisor(
-                    [args_ = std::make_tuple(std::forward<Args&&>(args)...)](actor_zeta::supervisor_abstract* ptr) {
-                        return detail::created_supervisor<ChildrenSupervisor>(ptr, args_, type_traits::make_index_sequence<number_of_arguments>{});
-                    })));
+    auto spawn_supervisor(base::supervisor& supervisor, Args&&... args) -> base::supervisor {
+        detail::spawn_supervisor_impl<ChildrenSupervisor>(supervisor->address(), supervisor->address(), std::forward<Args>(args)...);
     }
 
     template<
