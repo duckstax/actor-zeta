f550e679fb540aff46608a475ffe77ab51161b59
kotbegemot <k0tb9g9m0t@yandex.ru>
2021-09-14 01:57:19 +0300

    dynamic-spawn-supervisor fix build

--- examples/smart-send/main.cpp
+++ examples/smart-send/main.cpp
@@ -28,69 +28,6 @@ auto thread_pool_deleter = [](actor_zeta::abstract_executor* ptr) {
     delete ptr;
 };
 
-/// non thread safe
-class supervisor_lite final : public actor_zeta::supervisor_abstract {
-public:
-    explicit supervisor_lite()
-        : supervisor_abstract("network")
-        , e_(new actor_zeta::executor_t<actor_zeta::work_sharing>(
-                 1,
-                 100),
-             thread_pool_deleter)
-        , cursor(0)
-        , system_{
-              "sync_contacts",
-              "add_link",
-              "remove_link",
-              "spawn_actor"} {
-        e_->start();
-    }
-
-    ~supervisor_lite() override = default;
-
-    auto executor_impl() noexcept -> actor_zeta::abstract_executor* final { return e_.get(); }
-
-    auto add_actor_impl(actor_zeta::actor t) -> void final {
-        actors_.push_back(std::move(t));
-    }
-
-    auto add_supervisor_impl(actor_zeta::supervisor t) -> void final {
-        supervisor_.emplace_back(std::move(t));
-    }
-
-    auto enqueue_base(actor_zeta::message_ptr msg, actor_zeta::execution_device*) -> void final {
-        auto msg_ = std::move(msg);
-        auto it = system_.find(msg_->command());
-        if (it != system_.end()) {
-            local(std::move(msg_));
-        } else {
-            redirect_robin(std::move(msg_));
-        }
-    }
-
-private:
-    auto local(actor_zeta::message_ptr msg) -> void {
-        set_current_message(std::move(msg));
-        execute();
-    }
-
-    auto redirect_robin(actor_zeta::message_ptr msg) -> void {
-        if (!actors_.empty()) {
-            actors_[cursor]->enqueue(std::move(msg));
-            ++cursor;
-            if (cursor >= actors_.size()) {
-                cursor = 0;
-            }
-        }
-    }
-
-    std::unique_ptr<actor_zeta::abstract_executor, decltype(thread_pool_deleter)> e_;
-    std::vector<actor_zeta::actor> actors_;
-    std::vector<actor_zeta::supervisor> supervisor_;
-    std::size_t cursor;
-    std::unordered_set<actor_zeta::detail::string_view> system_;
-};
-
 struct download_data final {
     download_data(const std::string& url, const std::string& user, const std::string& passwod)
         : url_(url)
@@ -146,13 +83,82 @@ private:
     std::string tmp_;
 };
 
+/// non thread safe
+class supervisor_lite final : public actor_zeta::supervisor_abstract {
+public:
+    explicit supervisor_lite()
+        : supervisor_abstract("network")
+        , e_(new actor_zeta::executor_t<actor_zeta::work_sharing>(
+                 1,
+                 100),
+             thread_pool_deleter)
+        , cursor(0)
+        , system_{
+              "sync_contacts",
+              "add_link",
+              "remove_link",
+              "spawn_actor","create"} {
+        e_->start();
+        add_handler("create", &supervisor_lite::create);
+    }
+
+    void create() {
+        spawn_actor<worker_t>();
+    }
+
+    ~supervisor_lite() override = default;
+
+    auto executor_impl() noexcept -> actor_zeta::abstract_executor* final { return e_.get(); }
+
+    auto add_actor_impl(actor_zeta::actor t) -> void final {
+        actors_.push_back(std::move(t));
+    }
+
+    auto add_supervisor_impl(actor_zeta::supervisor t) -> void final {
+        supervisor_.emplace_back(std::move(t));
+    }
+
+    auto enqueue_base(actor_zeta::message_ptr msg, actor_zeta::execution_device*) -> void final {
+        auto msg_ = std::move(msg);
+        auto it = system_.find(msg_->command());
+        if (it != system_.end()) {
+            local(std::move(msg_));
+        } else {
+            redirect_robin(std::move(msg_));
+        }
+    }
+
+private:
+    auto local(actor_zeta::message_ptr msg) -> void {
+        set_current_message(std::move(msg));
+        execute();
+    }
+
+    auto redirect_robin(actor_zeta::message_ptr msg) -> void {
+        if (!actors_.empty()) {
+            actors_[cursor]->enqueue(std::move(msg));
+            ++cursor;
+            if (cursor >= actors_.size()) {
+                cursor = 0;
+            }
+        }
+    }
+
+    std::unique_ptr<actor_zeta::abstract_executor, decltype(thread_pool_deleter)> e_;
+    std::vector<actor_zeta::actor> actors_;
+    std::vector<actor_zeta::supervisor> supervisor_;
+    std::size_t cursor;
+    std::unordered_set<actor_zeta::detail::string_view> system_;
+};
+
+
 int main() {
     actor_zeta::supervisor supervisor(new supervisor_lite());
 
     int const actors = 10;
 
     for (auto i = actors - 1; i > 0; --i) {
-        actor_zeta::spawn_actor<worker_t>(supervisor);
+        actor_zeta::send(supervisor,actor_zeta::empty_address,"create");
     }
 
     //actor_zeta::spawn_actor<worker_t>(supervisor);
