e428ab68dfadb5375dad6f8100eeb55ded115d17
Alexander <k0tb9g9m0t@yandex.ru>
2022-03-03 15:39:43 +0300

    new body for massage (#90)
    * giving up a std::any
    #98
    
    Co-authored-by: gremsnoort <grem_snoort@protonmail.com>

--- benchmark/message/fixtures.hpp
+++ benchmark/message/fixtures.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <actor-zeta/detail/pmr/memory_resource.hpp>
+
 #include <iostream>
 #include <list>
 #include <map>
@@ -13,6 +15,11 @@
 
 namespace benchmark_messages {
 
+#if CPP17_OR_GREATER
+    std::unique_ptr<unsigned char[]> buffer = std::make_unique<unsigned char[]>(static_cast<size_t>(1073741824) * 1);
+    std::unique_ptr<unsigned char[]> backup = std::make_unique<unsigned char[]>(static_cast<size_t>(1073741824) * 2);
+#endif
+
     namespace by_name {
 
         template<typename P>
@@ -25,7 +32,7 @@ namespace benchmark_messages {
             }
 
             virtual void SetUp(::benchmark::State& state) final {
-                SetUp(state);
+                SetUp(static_cast<const ::benchmark::State&>(state));
             }
 
             virtual void TearDown(__attribute__((unused)) const ::benchmark::State& state) final {
@@ -33,7 +40,7 @@ namespace benchmark_messages {
             }
 
             virtual void TearDown(::benchmark::State& state) final {
-                TearDown(state);
+                TearDown(static_cast<const ::benchmark::State&>(state));
             }
 
             std::string name_;
@@ -50,18 +57,64 @@ namespace benchmark_messages {
             name_ = "name_";                                                                                                                  \
         }                                                                                                                                     \
         virtual void SetUp(::benchmark::State& state) final {                                                                                 \
-            SetUp(state);                                                                                                                     \
+            SetUp(static_cast<const ::benchmark::State&>(state));                                                                             \
         }                                                                                                                                     \
         virtual void TearDown(__attribute__((unused)) const ::benchmark::State& state) final {                                                \
             name_.clear();                                                                                                                    \
         }                                                                                                                                     \
         virtual void TearDown(::benchmark::State& state) final {                                                                              \
-            TearDown(state);                                                                                                                  \
+            TearDown(static_cast<const ::benchmark::State&>(state));                                                                          \
         }                                                                                                                                     \
         std::string name_;                                                                                                                    \
         std::tuple<CustomArgs...> arguments_;                                                                                                 \
         static constexpr size_t counter_ = sizeof...(CustomArgs);                                                                             \
     }
+
+    template<size_t N>
+    constexpr size_t getSize() {
+        return N;
+    }
+
+    template<size_t N, class Head, class... Args>
+    constexpr size_t getSize() {
+        return getSize<((N + alignof(Head) - 1) & -(alignof(Head))) + sizeof(Head), Args...>();
+    }
+
+#if CPP17_OR_GREATER
+#define DEFINE_FIXTURE_CLASS_MEM_RESOURCE_1(class_name, construct_name)                                                                       \
+    template<class P, typename... CustomArgs>                                                                                                 \
+    class class_name : public ::benchmark::Fixture {                                                                                          \
+        using monotonic_buffer_resource = actor_zeta::detail::pmr::monotonic_buffer_resource;                                                 \
+                                                                                                                                              \
+    public:                                                                                                                                   \
+        virtual void                                                                                                                          \
+        SetUp(const ::benchmark::State& state) final {                                                                                        \
+            arguments_ = std::make_tuple(std::forward<CustomArgs>(construct_name<CustomArgs>::get(static_cast<int64_t>(state.range(0))))...); \
+            static monotonic_buffer_resource upstr_tmp(benchmark_messages::backup.get(), size);                                               \
+            static monotonic_buffer_resource mr_tmp(benchmark_messages::buffer.get(), size, &upstr_tmp);                                      \
+            upstr = &upstr_tmp;                                                                                                               \
+            mr = &mr_tmp;                                                                                                                     \
+        }                                                                                                                                     \
+        virtual void SetUp(::benchmark::State& state) final {                                                                                 \
+            SetUp(static_cast<const ::benchmark::State&>(state));                                                                             \
+        }                                                                                                                                     \
+        virtual void TearDown(__attribute__((unused)) const ::benchmark::State& state) final {                                                \
+            mr->~monotonic_buffer_resource();                                                                                                 \
+            upstr->~monotonic_buffer_resource();                                                                                              \
+        }                                                                                                                                     \
+        virtual void TearDown(::benchmark::State& state) final {                                                                              \
+            TearDown(static_cast<const ::benchmark::State&>(state));                                                                          \
+        }                                                                                                                                     \
+        std::tuple<CustomArgs...> arguments_;                                                                                                 \
+        static constexpr size_t counter_ = sizeof...(CustomArgs);                                                                             \
+                                                                                                                                              \
+        static constexpr size_t size = getSize<0, CustomArgs...>() * 3;                                                                       \
+        monotonic_buffer_resource* upstr;                                                                                                     \
+        monotonic_buffer_resource* mr;                                                                                                        \
+    }
+#else
+#define DEFINE_FIXTURE_CLASS_MEM_RESOURCE_1(class_name, construct_name) DEFINE_FIXTURE_CLASS_1(class_name, construct_name)
+#endif
 
     namespace by_args {
 
@@ -78,6 +131,7 @@ namespace benchmark_messages {
             };
 
             DEFINE_FIXTURE_CLASS_1(fixture_t, construct);
+            DEFINE_FIXTURE_CLASS_MEM_RESOURCE_1(fixture_mem_resource_t, construct);
 
         } // namespace trivial_args
 
@@ -118,18 +172,54 @@ namespace benchmark_messages {
             name_ = "name_";                                                                                          \
         }                                                                                                             \
         virtual void SetUp(::benchmark::State& state) final {                                                         \
-            SetUp(state);                                                                                             \
+            SetUp(static_cast<const ::benchmark::State&>(state));                                                     \
         }                                                                                                             \
         virtual void TearDown(__attribute__((unused)) const ::benchmark::State& state) final {                        \
             name_.clear();                                                                                            \
         }                                                                                                             \
         virtual void TearDown(::benchmark::State& state) final {                                                      \
-            TearDown(state);                                                                                          \
+            TearDown(static_cast<const ::benchmark::State&>(state));                                                  \
         }                                                                                                             \
         std::string name_;                                                                                            \
         typename revert_type_seq<CustomArgs...>::type arguments_;                                                     \
         static constexpr size_t counter_ = sizeof...(CustomArgs);                                                     \
     }
+
+#if CPP17_OR_GREATER
+#define DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(class_name, construct_name, revert_type_seq)                              \
+    template<class P, typename... CustomArgs>                                                                         \
+    class class_name : public ::benchmark::Fixture {                                                                  \
+        using monotonic_buffer_resource = actor_zeta::detail::pmr::monotonic_buffer_resource;                         \
+                                                                                                                      \
+    public:                                                                                                           \
+        virtual void                                                                                                  \
+        SetUp(const ::benchmark::State& state) final {                                                                \
+            arguments_ = std::make_tuple((construct_name<CustomArgs>::get(static_cast<int64_t>(state.range(0))))...); \
+            static monotonic_buffer_resource upstr_tmp(benchmark_messages::backup.get(), size);                       \
+            static monotonic_buffer_resource mr_tmp(benchmark_messages::buffer.get(), size, &upstr_tmp);              \
+            upstr = &upstr_tmp;                                                                                       \
+            mr = &mr_tmp;                                                                                             \
+        }                                                                                                             \
+        virtual void SetUp(::benchmark::State& state) final {                                                         \
+            SetUp(static_cast<const ::benchmark::State&>(state));                                                     \
+        }                                                                                                             \
+        virtual void TearDown(__attribute__((unused)) const ::benchmark::State& state) final {                        \
+            mr->~monotonic_buffer_resource();                                                                         \
+            upstr->~monotonic_buffer_resource();                                                                      \
+        }                                                                                                             \
+        virtual void TearDown(::benchmark::State& state) final {                                                      \
+            TearDown(static_cast<const ::benchmark::State&>(state));                                                  \
+        }                                                                                                             \
+        typename revert_type_seq<CustomArgs...>::type arguments_;                                                     \
+        static constexpr size_t counter_ = sizeof...(CustomArgs);                                                     \
+                                                                                                                      \
+        static constexpr size_t size = getSize<0, CustomArgs...>() * 3;                                               \
+        monotonic_buffer_resource* upstr;                                                                             \
+        monotonic_buffer_resource* mr;                                                                                \
+    }
+#else
+#define DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(class_name, construct_name, revert_type_seq) DEFINE_FIXTURE_CLASS_2(class_name, construct_name, revert_type_seq)
+#endif
 
         namespace smart_pointer_args {
 
@@ -224,6 +314,11 @@ namespace benchmark_messages {
             DEFINE_FIXTURE_CLASS_2(fixture_list_t, construct_list, revert_type_seq_list);
             DEFINE_FIXTURE_CLASS_2(fixture_map_t, construct_map, revert_type_seq_map);
             DEFINE_FIXTURE_CLASS_2(fixture_set_t, construct_set, revert_type_seq_set);
+
+            DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(fixture_mr_vec_t, construct_vec, revert_type_seq_vec);
+            DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(fixture_mr_list_t, construct_list, revert_type_seq_list);
+            DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(fixture_mr_map_t, construct_map, revert_type_seq_map);
+            DEFINE_FIXTURE_CLASS_MEM_RESOURCE_2(fixture_mr_set_t, construct_set, revert_type_seq_set);
 
         } // namespace container_args
 
--- header/actor-zeta/make_message.hpp
+++ header/actor-zeta/make_message.hpp
@@ -16,12 +16,12 @@ namespace actor_zeta {
 
     template<class T, typename Arg>
     auto make_message_ptr(base::address_t sender_, T name, Arg&& arg) -> base::message* {
-        return new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::any(std::forward<type_traits::decay_t<Arg>>(arg))));
+        return new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::rtt(nullptr, std::forward<type_traits::decay_t<Arg>>(arg))));
     }
 
     template<class T, typename... Args>
     auto make_message_ptr(base::address_t sender_, T name, Args&&... args) -> base::message* {
-        return new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::any(std::tuple<type_traits::decay_t<Args>...>{std::forward<Args>(args)...})));
+        return new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::rtt(nullptr, std::forward<Args>(args)...)));
     }
 
     template<class T>
@@ -31,12 +31,12 @@ namespace actor_zeta {
 
     template<class T, typename Arg>
     auto make_message(base::address_t sender_, T name, Arg&& arg) -> base::message_ptr {
-        return base::message_ptr(new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::any(std::forward<type_traits::decay_t<Arg>>(arg)))));
+        return base::message_ptr(new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::rtt(nullptr, std::forward<type_traits::decay_t<Arg>>(arg)))));
     }
 
     template<class T, typename... Args>
     auto make_message(base::address_t sender_, T name, Args&&... args) -> base::message_ptr {
-        return base::message_ptr(new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::any(std::tuple<type_traits::decay_t<Args>...>{std::forward<Args>(args)...}))));
+        return base::message_ptr(new base::message(std::move(sender_), std::forward<T>(name), std::move(detail::rtt(nullptr, std::forward<Args>(args)...))));
     }
 
 } // namespace actor_zeta
