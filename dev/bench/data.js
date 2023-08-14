2aa014c3364109d4b3debcee8dd22fb8b44de5ba
github-action-benchmark <github@users.noreply.github.com>
2023-08-14 17:39:18 +0000

    add C++ Benchmark (googlecpp) benchmark result for 299e78cfacf112c13e24b7a10a15c16d79cfc8d2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692034757932,
+  "lastUpdate": 1692034758945,
   "repoUrl": "https://github.com/duckstax/actor-zeta",
   "entries": {
     "C++ Benchmark": [
@@ -863,6 +863,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/duckstax/actor-zeta/commit/299e78cfacf112c13e24b7a10a15c16d79cfc8d2"
         },
         "date": 1692034751798,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fixture_t<int8_t>/ping_pong_single_threaded_1_int8_t/0",
+            "value": 26947.214220373633,
+            "unit": "ns/iter",
+            "extra": "iterations: 91826\ncpu: 8762.739311306168 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t>/ping_pong_single_threaded_2_int8_t/0",
+            "value": 23349.359130000383,
+            "unit": "ns/iter",
+            "extra": "iterations: 100000\ncpu: 7382.796999999999 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t>/ping_pong_single_threaded_3_int8_t/0",
+            "value": 28067.144894990386,
+            "unit": "ns/iter",
+            "extra": "iterations: 78374\ncpu: 9021.1575267308 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_4_int8_t/0",
+            "value": 27909.630510480245,
+            "unit": "ns/iter",
+            "extra": "iterations: 74381\ncpu: 9002.92278942203 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_5_int8_t/0",
+            "value": 28274.546505517756,
+            "unit": "ns/iter",
+            "extra": "iterations: 78023\ncpu: 9144.211322302399 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_6_int8_t/0",
+            "value": 28210.8939277774,
+            "unit": "ns/iter",
+            "extra": "iterations: 78258\ncpu: 8970.08229190626 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_7_int8_t/0",
+            "value": 27964.147748178613,
+            "unit": "ns/iter",
+            "extra": "iterations: 79358\ncpu: 9084.878651175683 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_8_int8_t/0",
+            "value": 27772.20666026765,
+            "unit": "ns/iter",
+            "extra": "iterations: 75012\ncpu: 9028.491441369377 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_9_int8_t/0",
+            "value": 27467.607115644052,
+            "unit": "ns/iter",
+            "extra": "iterations: 102366\ncpu: 8924.052908192172 ns\nthreads: 1"
+          },
+          {
+            "name": "fixture_t<int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t, int8_t>/ping_pong_single_threaded_10_int8_t/0",
+            "value": 26693.602897639386,
+            "unit": "ns/iter",
+            "extra": "iterations: 76338\ncpu: 8568.884435012696 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "grem_snoort@protonmail.com",
+            "name": "GremSnoort",
+            "username": "GremSnoort"
+          },
+          "committer": {
+            "email": "grem_snoort@protonmail.com",
+            "name": "GremSnoort",
+            "username": "GremSnoort"
+          },
+          "distinct": true,
+          "id": "299e78cfacf112c13e24b7a10a15c16d79cfc8d2",
+          "message": "extreme check 6 runs-on: ubuntu-",
+          "timestamp": "2023-08-14T20:38:52+03:00",
+          "tree_id": "9f0312b15bf765d3051aed8642b09c4ffa3bcc49",
+          "url": "https://github.com/duckstax/actor-zeta/commit/299e78cfacf112c13e24b7a10a15c16d79cfc8d2"
+        },
+        "date": 1692034752054,
         "tool": "googlecpp",
         "benches": [
           {
